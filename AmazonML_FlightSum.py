2a7b02f29b6fc2db82bc8ef6da29ed048aedafac
CVr Kaushik Narayanan <90693611+CVrKaushikNarayanan@users.noreply.github.com>
2024-06-16 11:10:51 +0530

    Add files via upload

---
+++ AmazonML_FlightSum.py
@@ -0,0 +1,31 @@
+n = input("list")
+k1 = list(map(int,input().split()))
+def mini(i,n):
+    if(i+3<len(n)):
+        a=abs(abs(n[i])-abs(n[i+1]))
+        b=abs(abs(n[i])-abs(n[i+3]))
+        if(a<b):
+            return i+1,a
+        elif(b<a):
+            return i+3,b
+        else:
+            i1,c1 = mini(i+1,n)
+            i2,c2 = mini(i+3,n)
+            if c1<c2:
+                return i1,c1+a
+            else:
+                return i2,c2+b
+    else:
+        return i+1,abs(abs(n[i])-abs(n[i+1]))
+
+    
+def sd(n):
+    c = 0
+    i = 0
+    while(i<len(n)-1):
+        i,b = mini(i,n)
+        c+=b
+        #print(i,c)
+    print(c)
+k = [1,4 ,5,2]
+sd(k)
